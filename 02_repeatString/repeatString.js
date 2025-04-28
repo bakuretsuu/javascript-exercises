4be558996ec0b8e30d01ea83412d2daa675e5b36
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-04-29 06:19:07 +0800

    completed exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let repeatedWord = "";
+    if(times < 0){return "ERROR"}
+    else{
+    for(let i = 0; i < times; i++){
+    repeatedWord += word;}}
+  
+    return repeatedWord;
 };
 
 // Do not edit below this line
