ec3330cd2d216e2cdec033e152b327b4bb2e6573
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-04-30 06:39:46 +0800

    completed exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(word) {
+let reversedString = word.split("").reverse().join("");
+return reversedString;
+}
 // Do not edit below this line
 module.exports = reverseString;
