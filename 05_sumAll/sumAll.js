e7a80d7dd1cc83a4c217a953c6717a8a2ae301a9
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-04-30 08:57:11 +0800

    complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber,secondNumber) {
+    let largeNumber = 0;
+    let smallNumber = 0;
+   
+    if (firstNumber > secondNumber){
+    largeNumber = firstNumber;
+    smallNumber = secondNumber;
+   }
+   else{
+    largeNumber = secondNumber;
+    smallNumber = firstNumber;
+   }
+    sum = 0;
+    if(smallNumber < 0 || largeNumber < 0){
+        return "ERROR";
+    }
+    else if (!Number.isInteger(smallNumber) || !Number.isInteger(largeNumber)){
+        return "ERROR";
+    }
+    else{for(let i = smallNumber; i <= largeNumber; i++){
+        sum += i;
+    }}
+    return sum;
 };
 
 // Do not edit below this line
