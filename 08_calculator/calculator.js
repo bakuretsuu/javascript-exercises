c4e36249d537f85e8d69094e28fdeed55bb6be46
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-05-11 16:33:43 +0800

    complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((num,sum = 0) => sum = sum + num, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr){
+  return arr.reduce((multiplied, current) => multiplied * current, 1);
+}
 
+const power = function(a,b) {
+	return Math.pow(a, b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+  for (let i = num; i > 0; i--){
+    factorial *= i;
+  }
+	return factorial;
 };
 
 // Do not edit below this line
