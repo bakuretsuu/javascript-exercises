fd653bbd21ef4f73c3b56a53c0dcb5def9a6b34f
bakuretsuu <190571925+bakuretsuu@users.noreply.github.com>
2025-05-11 16:54:34 +0800

    complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [0, 1];
 
+    if (num < 0){
+        return "OOPS";
+    }
+    else
+   { for (i = 2; i <= num; i++) {
+        // Next fibonacci number = previous + one before previous
+        // Translated to JavaScript:
+        fib[i] = fib[i - 2] + fib[i - 1];
+      }
+   }
+      return fib[num];
 };
 
 // Do not edit below this line
